886b8ebae44a23c10a023f3cc98702bea3d3752e
ilopjim1306 <ljivan@iesrafaelalberti.es>
2024-09-26 20:31:12 +0200

    terminado

--- src/main/kotlin/repository/CompanyRepository.kt
+++ src/main/kotlin/repository/CompanyRepository.kt
@@ -1,5 +1,7 @@
 package org.example.repository
 
+import java.io.BufferedReader
+import java.io.BufferedWriter
 import java.nio.file.Files
 import java.nio.file.Path
 import java.nio.file.StandardOpenOption
@@ -17,8 +19,8 @@ class CompanyRepository(private val fileDir:Path) {
         val br = Files.newBufferedReader(fileDir)
         br.use {
             it.forEachLine { line ->
-                val lineCompany = line.split(";")
-                comapnyMap[lineCompany[0]] = listOf(lineCompany[1], lineCompany[2],lineCompany[3],lineCompany[4],lineCompany[5])
+                val lineCompany = line.replace(",", ".").split(";")
+                if (lineCompany[1].toFloatOrNull() != null) comapnyMap[lineCompany[0]] = listOf(lineCompany[1], lineCompany[2],lineCompany[3],lineCompany[4],lineCompany[5])
             }
         }
         return comapnyMap
@@ -27,21 +29,30 @@ class CompanyRepository(private val fileDir:Path) {
 
     fun createFile(companyMap:Map<String, List<String>>, newFileDir:Path) {
 
+        val bw = Files.newBufferedWriter(newFileDir, StandardOpenOption.APPEND)
+
         if (newFileDir.notExists()) {
             Files.createFile(newFileDir)
         }
+        val br = Files.newBufferedReader(newFileDir)
 
-        val bw = Files.newBufferedWriter(newFileDir, StandardOpenOption.APPEND)
         bw.use { flujo ->
+            checkTittle(br, flujo)
             var media:Float
             companyMap.forEach{ map ->
-                if(map.value[1].replace(",", ".").toFloatOrNull() != null) {
-                    val max = map.value[1].replace(",", ".").toFloat()
-                    val min = map.value[2].replace(",", ".").toFloat()
-                    media = (max + min) /2
-                    flujo.write("${map.key};${map.value[1]};${map.value[2]};$media")
-                } else flujo.write("${map.key};${map.value[1]};${map.value[2]};Media")
+                val max = map.value[1].toFloat()
+                val min = map.value[2].toFloat()
+                media = (max + min) /2
+                flujo.write("${map.key};${map.value[1]};${map.value[2]};$media")
+                flujo.newLine()
+            }
+        }
+    }
 
+    private fun checkTittle(br:BufferedReader, flujo:BufferedWriter) {
+        br.use {
+            if (it.lines().toList().isEmpty()) {
+                flujo.write("Nombre;Máximo;Mínimo;Media")
                 flujo.newLine()
             }
         }
